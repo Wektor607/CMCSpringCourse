d4f47225cb6a6b1ffad8fefdf46279affb6ba6eb
Wektor607 <1069035F@mail.ru>
2022-12-30 13:33:12 +0300

    Спецкурс "Иновационные корпаративные системы"

--- src/main/java/ru/mikhelson/springcourse/SpringApp/controllers/AuthController.java
+++ src/main/java/ru/mikhelson/springcourse/SpringApp/controllers/AuthController.java
@@ -1,16 +1,7 @@
 package ru.mikhelson.springcourse.SpringApp.controllers;
 
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.mikhelson.springcourse.SpringApp.models.Person;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
